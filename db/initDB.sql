dae456b03f80fe6f45bf3db2ebe98bac9236203d
hsingh0825 <harmeets0825@gmail.com>
2021-05-06 01:05:25 -0700

    Added SQL statements to initalize DB

--- db/initDB.sql
+++ db/initDB.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS wsi_stack;
 USE wsi_stack;
 
 DROP TABLE IF EXISTS pt_header;
+DROP TABLE IF EXISTS pt_detail;
+
 CREATE TABLE pt_header (
 	pick_ticket_num VARCHAR(30) NOT NULL UNIQUE,
     order_num VARCHAR(30) NOT NULL,
@@ -14,7 +16,7 @@ CREATE TABLE pt_header (
     sold_to_state VARCHAR(36),
     sold_to_country VARCHAR(36),
     sold_to_zip VARCHAR(15),
-    ship_to VARCHAR(30) NOT NULL,
+    ship_to VARCHAR(30) DEFAULT "",
     ship_to_name VARCHAR(128) NOT NULL,
     ship_to_address VARCHAR(128) NOT NULL,
     ship_to_city VARCHAR(30) NOT NULL,
@@ -25,15 +27,17 @@ CREATE TABLE pt_header (
     PRIMARY KEY (pick_ticket_num)
 );
 
-DROP TABLE IF EXISTS pt_detail;
 CREATE TABLE pt_detail (
-    pick_ticket_num VARCHAR(30) NOT NULL UNIQUE,
+    pick_ticket_num VARCHAR(30) NOT NULL,
     line_no INT NOT NULL,
     sku_1 VARCHAR(60) NOT NULL,
     units_ordered INT NOT NULL,
     units_to_ship INT NOT NULL,
-    unit_price DECIMAL(2),
+    unit_price FLOAT,
     date_last_modified DATETIME NOT NULL,
+    PRIMARY KEY (pick_ticket_num),
     CONSTRAINT detail_to_header FOREIGN KEY (pick_ticket_num)
     REFERENCES pt_header(pick_ticket_num)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
 );
