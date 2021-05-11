1986b1194b9ced93b86457362f73fd43c50165fa
hsingh0825 <harmeets0825@gmail.com>
2021-05-10 20:57:12 -0700

    Updated tables to include unique indices

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -3,23 +3,27 @@ CREATE DATABASE IF NOT EXISTS wsi_stack;
 USE wsi_stack;
 
 CREATE TABLE customer(
-	sold_to_name VARCHAR(128) PRIMARY KEY,
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+	sold_to_name VARCHAR(128),
 	sold_to_id VARCHAR(30) DEFAULT "",
 	sold_to_address_1 VARCHAR(128) NOT NULL,
 	sold_to_city VARCHAR(30) NOT NULL,
 	sold_to_state VARCHAR(36) NOT NULL,
 	sold_to_country VARCHAR(36) NOT NULL,
-	sold_to_zip VARCHAR(15) NOT NULL
+	sold_to_zip VARCHAR(15) NOT NULL,
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE recipient(
-	ship_to_name VARCHAR(128) PRIMARY KEY,
+	recipient_id INT AUTO_INCREMENT PRIMARY KEY,
+	ship_to_name VARCHAR(128),
 	ship_to_id VARCHAR(30) DEFAULT "",
 	ship_to_address VARCHAR(128) NOT NULL,
 	ship_to_city VARCHAR(30) NOT NULL,
 	ship_to_state VARCHAR(36) NOT NULL,
 	ship_to_country VARCHAR(36) NOT NULL,
-	ship_to_zip VARCHAR(15) NOT NULL
+	ship_to_zip VARCHAR(15) NOT NULL,
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE pick_ticket_header(
@@ -29,14 +33,14 @@ CREATE TABLE pick_ticket_header(
 
 CREATE TABLE `order`(
 	order_num VARCHAR(30) PRIMARY KEY,
-	sold_to VARCHAR(128),
-	ship_to VARCHAR(128),
+	sold_to INT,
+	ship_to INT,
 	ship_method VARCHAR(5) NOT NULL,
 	CONSTRAINT orderToCustomer FOREIGN KEY (sold_to)
-		REFERENCES customer(sold_to_name)
+		REFERENCES customer(customer_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT orderToRecipient FOREIGN KEY (ship_to)
-		REFERENCES recipient(ship_to_name)
+		REFERENCES recipient(recipient_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT orderToHeader FOREIGN KEY (order_num)
 		REFERENCES pick_ticket_header(order_num)
@@ -75,8 +79,8 @@ CREATE TABLE line_item(
 CREATE TABLE shipping_conf(
 	pick_ticket_num VARCHAR(30),
 	line_num INT,
-	tracking_num TEXT DEFAULT "Not yet shipped",
 	ship_date DATETIME DEFAULT NULL,
+    tracking_num TEXT,
 	PRIMARY KEY (pick_ticket_num, line_num),
 	CONSTRAINT confToTicket FOREIGN KEY (pick_ticket_num)
 		REFERENCES pick_ticket_header(pick_ticket_num)
