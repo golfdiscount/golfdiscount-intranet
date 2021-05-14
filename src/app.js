d90fbd839ed6fc3c9983a22896dfd0245c60b9af
hsingh0825 <harmeets0825@gmail.com>
2021-05-13 17:47:24 -0700

    Initialized entry point for API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-/*
-Contains the implementation to
-*/
-
 "use strict";
 
 const express = require("express");
 const app = express();
-require("./routes/routes")(app);
 const multer = require("multer");
+let db = require("./db");
 
 app.use(multer().none())
-
 app.use(express.static("public"));
-const PORT = process.env.PORT || 8000;
+app.use(express.json())
+require("./routes/routes")(app);
 
+db.connect();
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`API up and running on port ${PORT}`)
 })
