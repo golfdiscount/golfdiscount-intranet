caf600da71abd5a0b7a92d232ab69e56a77b1f51
hsingh0825 <harmeets0825@gmail.com>
2021-05-19 15:52:20 -0700

    Created initial website implementation

--- src/public/index.js
+++ src/public/index.js
@@ -3,14 +3,6 @@
 (function() {
   const DOMAIN = 'localhost:3000/'
 
-  window.addEventListener("load", init);
-
-  /*
-  Set up initial state of window
-  */
- function init() {
-  let p = document.createElement("p")
-  p.textContent = "Window successfully loaded";
-  document.querySelector("main").appendChild(p)
- }
+  window.addEventListener("load", () => {
+  });
 })();
