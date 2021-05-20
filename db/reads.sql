069a82d1a571049f2b0309928a25edef3d0a9fc4
Harmeet Singh <harmeet@golfdiscount.com>
2021-05-20 11:35:40 -0700

    Added a query to select order information

--- db/reads.sql
+++ db/reads.sql
@@ -7,3 +7,24 @@ SELECT c.sold_to_name AS "Name",
   c.sold_to_country AS "Country",
   c.sold_to_zip AS "Zip"
 FROM customer AS c
+
+/*
+Get information about all orders
+*/
+SELECT wsi_order.order_num AS "Order Number",
+	c.sold_to_name AS "Customer Name",
+  c.sold_to_address AS "Customer Address",
+  c.sold_to_city AS "Customer City",
+  c.sold_to_state AS "Customer State",
+  c.sold_to_country AS "Customer Country",
+  c.sold_to_zip AS "Customer Zip",
+  r.ship_to_name AS "Recipient Name",
+  r.ship_to_address AS "Recipient Address",
+  r.ship_to_city AS "Recipient City",
+  r.ship_to_state AS "Recipient State",
+  r.ship_to_country AS "Recipient Country",
+  r.ship_to_zip AS "Recipient Zip"
+FROM wsi_order
+JOIN pick_ticket_header AS pth ON pth.order_num = wsi_order.order_num
+JOIN customer AS c ON c.customer_id = wsi_order.sold_to
+JOIN recipient AS r ON r.recipient_id = wsi_order.ship_to;
