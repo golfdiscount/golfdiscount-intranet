a5d955f6c2ca331294b76c07bf714c7f661c24b5
Harmeet Singh <harmeet@golfdiscount.com>
2021-05-24 16:13:23 -0700

    Added basic fetch coded

--- src/public/index.js
+++ src/public/index.js
@@ -1,8 +1,27 @@
 "use strict";
 
 (function() {
-  const DOMAIN = 'localhost:3000/'
+  const DOMAIN = 'http://localhost:8000'
 
   window.addEventListener("load", () => {
+    idSel("search").addEventListener('submit', searchOrder);
   });
+
+  function searchOrder(e) {
+    let url = new URL(DOMAIN + "/test")
+
+    fetch(url, {mode: 'no-cors'})
+      .then(res => res.text())
+      .then(res => {
+        console.log(res)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    e.preventDefault();
+  }
+
+  function idSel(id) {
+    return document.getElementById(id)
+  }
 })();
