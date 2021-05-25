3a979fd28f367ea900af89177e1859ab11917db3
hsingh0825 <harmeets0825@gmail.com>
2021-05-24 22:26:30 -0700

    Added function  to display information

--- src/public/index.js
+++ src/public/index.js
@@ -4,20 +4,59 @@
   const DOMAIN = 'http://localhost:8000'
 
   window.addEventListener("load", () => {
-    idSel("search").addEventListener('submit', searchOrder);
+    id("search").addEventListener('submit', searchOrder);
   });
 
   async function searchOrder(e) {
     e.preventDefault();
-    let url = new URL(DOMAIN + "/orders/1000692635")
+    let url = new URL(DOMAIN + "/orders/" + qs("#order-num").value)
 
-    let order =
-    await fetch(url, {mode: 'no-cors'})
+    let order = await fetch(url, {mode: 'no-cors'})
       .then(res => res.json())
       .catch(e => {
         console.log(e)
       })
-    console.log(order.SKU)
+
+    displayOrder(order)
+  }
+
+  function displayOrder(order) {
+    // Customer info
+    qs("#c-name output").textContent = order.sold_to_name
+    qs("#c-address output").textContent = order.sold_to_address
+    qs("#c-city output").textContent = order.sold_to_city
+    qs("#c-state output").textContent = order.sold_to_state
+    qs("#c-country output").textContent = order.sold_to_country
+    qs("#c-zip output").textContent = order.sold_to_zip
+
+    // Recipient info
+    qs("#r-name output").textContent = order.ship_to_name
+    qs("#r-address output").textContent = order.ship_to_address
+    qs("#r-city output").textContent = order.ship_to_city
+    qs("#r-state output").textContent = order.ship_to_state
+    qs("#r-country output").textContent = order.ship_to_country
+    qs("#r-zip output").textContent = order.ship_to_zip
+
+    // Product info
+    qs("#sku-name output").textContent = order.sku_name
+    qs("#sku output").textContent = order.sku
+    qs("#price output").textContent = order.unit_price
+    qs("#quantity output").textContent = order.quantity
+
+    // Shipping info
+    let ship_date_field = qs("#ship-date output")
+    if (order.ship_date == null) {
+      ship_date_field.textContent = "Not yet uploaded"
+    } else {
+      ship_date_field.textContent = order.ship_date
+    }
+
+    let tracking_field = qs("#tracking output")
+    if (order.tracking_num == null) {
+      tracking_field.textContent = "Not yet uploaded"
+    } else {
+      tracking_field = order.tracking_num
+    }
   }
 
   /**
