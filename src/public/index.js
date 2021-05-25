11018eece61c81f1559f0f5422e63afde28feb4c
hsingh0825 <harmeets0825@gmail.com>
2021-05-24 19:26:06 -0700

    Added helper functions for DOM manipulation

--- src/public/index.js
+++ src/public/index.js
@@ -17,11 +17,57 @@
       .catch(e => {
         console.log(e)
       })
-    
     console.log(order.SKU)
   }
 
-  function idSel(id) {
-    return document.getElementById(id)
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+   function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  function checkStatus(response) {
+    if (response.ok) {
+      return response;
+    } else {
+      throw Error("Error in request: " + response.statusText);
+    }
+  }
+
+  /**
+   * Appends a node to the given parent node
+   * @param {string} parent - The parent node
+   * @param {string} child - The child node to be appended to parent node
+   */
+  function append(parent, child) {
+    parent.appendChild(child);
   }
 })();
