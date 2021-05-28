0721322e537c1c4868062b611b355b98c794c5ff
Harmeet Singh <harmeet@golfdiscount.com>
2021-05-28 13:06:08 -0700

    Added SSL certification

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 let fs = require("fs")
+
 dbConnection = {
   host: "wsi-orders.mysql.database.azure.com",
   user: "harmeet@wsi-orders",
   password: "%a5xe2*S4xxd^MDmyqFFd!qrwSGKBPN&QCPiXtDV&&=xE7=SyumA%>:zLGEuo].E",
   database: 'wsi_stack',
   multipleStatements: true,
+  ssl: {
+    cert: fs.readFileSync('BaltimoreCyberTrustRoot.crt.pem'),
+    rejectUnauthorized: true
+  }
 }
 
 let mysql = require("mysql2");
@@ -13,7 +18,7 @@ let connection = mysql.createConnection(dbConnection)
 exports.connect = function () {
   connection.connect((err) => {
     if (err) {
-      console.error('error connectiong:\n' + err.stack);
+      console.error('error connecting:\n' + err.stack);
       return;
     }
     console.log('connected as id ' + connection.threadId);
