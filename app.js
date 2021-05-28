3817f2b87a3ecf1fc49635189b122c15c01d6f1a
Harmeet Singh <harmeet@golfdiscount.com>
2021-05-28 09:02:11 -0700

    Updated directory structure

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-"use strict";
-
 const express = require("express");
 const app = express();
+
 const multer = require("multer");
 let db = require("./db");
 
 app.use(multer().none())
 app.use(express.static("public"));
 app.use(express.json())
-require("./routes/routes")(app);
+//require("./routes/routes")(app);
 
 db.connect();
 
 const PORT = process.env.PORT | 8000;
-const HOSTNAME = process.env.HOSTNAME | "wsi-orders.mysql.database.azure.com";
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`API up and running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Testing app on port ${PORT}`)
 })
