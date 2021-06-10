0c7695ea0df556807cc87c81f56b9ad7c55e64d0
Harmeet Singh <harmeet@golfdiscount.com>
2021-06-10 10:35:36 -0700

    Refactored code and updated connectors

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT | 8080;
-const multer = require("multer");
+const PORT = process.env.PORT | 8000;
 
 let db = require("./db");
 
-app.use(multer().none())
 app.use(express.static("public"));
 app.use(express.json())
 require("./routes/routes")(app);
-
-app.get('/testRoute', (req, res) => {
-  res.status(200).send('This route is working')
-})
 
 db.connect();
 
--- db.js
+++ db.js
@@ -2,9 +2,9 @@ let fs = require("fs")
 
 dbConnection = {
   host: "wsi-orders.mysql.database.azure.com",
-  user: "harmeet@wsi-orders",
+  user: "harmeet_pgd@wsi-orders",
   password: "%a5xe2*S4xxd^MDmyqFFd!qrwSGKBPN&QCPiXtDV&&=xE7=SyumA%>:zLGEuo].E",
-  database: 'wsi_stack',
+  database: 'wsi_orders',
   multipleStatements: true,
   ssl: {
     cert: fs.readFileSync('BaltimoreCyberTrustRoot.crt.pem'),
@@ -13,7 +13,7 @@ dbConnection = {
 }
 
 let mysql = require("mysql2");
-let connection = mysql.createConnection(dbConnection)
+let connection = mysql.createConnection(dbConnection);
 
 exports.connect = function () {
   connection.connect((err) => {
@@ -22,7 +22,7 @@ exports.connect = function () {
       return;
     }
     console.log('connected as id ' + connection.threadId);
-  })
+  });
 }
 
 exports.executeQuery = function(query, handler) {
