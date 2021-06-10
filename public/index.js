d34b9050a3c11e39d1d44c7deee58b120b54ca96
Harmeet Singh <harmeet@golfdiscount.com>
2021-06-10 10:35:14 -0700

    Updated nav bar and added a shipping confirmation section

--- public/index.js
+++ public/index.js
@@ -1,15 +1,25 @@
-"use strict";
+'use strict';
 
 (function() {
-  const DOMAIN = 'http://wsistack-env.eba-vyfp9vy3.us-west-2.elasticbeanstalk.com:8080'
+  const DOMAIN = 'http://localhost:8000'
 
-  window.addEventListener("load", () => {
-    id("search").addEventListener('submit', searchOrder);
+  /**
+   * Sets initial state of the window and adds necessary listeners
+   */
+  window.addEventListener('load', () => {
+    id('search').addEventListener('submit', searchOrder);
+    id('orders').addEventListener('click', showOrderViewer);
+    id('shipping').addEventListener('click', showShippingConf);
+    id('submit-file').addEventListener('click', submitConf);
   });
 
+  /**
+   * Queries API to search for an order
+   * @param {DOM Object} e Object originating callback 
+   */
   async function searchOrder(e) {
     e.preventDefault();
-    let url = new URL(DOMAIN + "/orders/" + qs("#order-num").value)
+    let url = new URL(DOMAIN + '/orders/' + qs('#order-num').value)
 
     let order = await fetch(url, {mode: 'no-cors'})
       .then(res => res.json())
@@ -20,43 +30,73 @@
     displayOrder(order)
   }
 
+  /**
+   * Renders order information to be displayed on screen
+   * @param {object} order JSON object containing order information 
+   */
   function displayOrder(order) {
     // Customer info
-    qs("#c-name output").textContent = order.sold_to_name
-    qs("#c-address output").textContent = order.sold_to_address
-    qs("#c-city output").textContent = order.sold_to_city
-    qs("#c-state output").textContent = order.sold_to_state
-    qs("#c-country output").textContent = order.sold_to_country
-    qs("#c-zip output").textContent = order.sold_to_zip
+    qs('#c-name output').textContent = order.sold_to_name
+    qs('#c-address output').textContent = order.sold_to_address
+    qs('#c-city output').textContent = order.sold_to_city
+    qs('#c-state output').textContent = order.sold_to_state
+    qs('#c-country output').textContent = order.sold_to_country
+    qs('#c-zip output').textContent = order.sold_to_zip
 
     // Recipient info
-    qs("#r-name output").textContent = order.ship_to_name
-    qs("#r-address output").textContent = order.ship_to_address
-    qs("#r-city output").textContent = order.ship_to_city
-    qs("#r-state output").textContent = order.ship_to_state
-    qs("#r-country output").textContent = order.ship_to_country
-    qs("#r-zip output").textContent = order.ship_to_zip
+    qs('#r-name output').textContent = order.ship_to_name
+    qs('#r-address output').textContent = order.ship_to_address
+    qs('#r-city output').textContent = order.ship_to_city
+    qs('#r-state output').textContent = order.ship_to_state
+    qs('#r-country output').textContent = order.ship_to_country
+    qs('#r-zip output').textContent = order.ship_to_zip
 
     // Product info
-    qs("#sku-name output").textContent = order.sku_name
-    qs("#sku output").textContent = order.sku
-    qs("#price output").textContent = order.unit_price
-    qs("#quantity output").textContent = order.quantity
+    qs('#sku-name output').textContent = order.sku_name
+    qs('#sku output').textContent = order.sku
+    qs('#price output').textContent = order.unit_price
+    qs('#quantity output').textContent = order.quantity
 
     // Shipping info
-    let ship_date_field = qs("#ship-date output")
+    let ship_date_field = qs('#ship-date output')
     if (order.ship_date == null) {
-      ship_date_field.textContent = "Not yet uploaded"
+      ship_date_field.textContent = 'Not yet uploaded'
     } else {
       ship_date_field.textContent = order.ship_date
     }
 
-    let tracking_field = qs("#tracking output")
+    let tracking_field = qs('#tracking output')
     if (order.tracking_num == null) {
-      tracking_field.textContent = "Not yet uploaded"
+      tracking_field.textContent = 'Not yet uploaded'
     } else {
       tracking_field = order.tracking_num
     }
+  }
+
+  /**
+   * Submits a file to the WSI API
+   * @param {DOM object} e Object originating callback
+   */
+  function submitConf(e) {
+    alert('File has been submitted');
+  }
+
+  /**
+   * Shows the order viewing window
+   * @param {DOM Object} e Object originating callback
+   */
+  function showOrderViewer(e) {
+    id('order-viewer').classList.remove('hidden');
+    id('shipping-conf').classList.add('hidden');
+  }
+
+  /**
+   * Shows the shipping confirmation menu
+   * @param {DOM Object} e Object originating callback
+   */
+  function showShippingConf(e) {
+    id('order-viewer').classList.add('hidden');
+    id('shipping-conf').classList.remove('hidden');
   }
 
   /**
@@ -97,7 +137,7 @@
     if (response.ok) {
       return response;
     } else {
-      throw Error("Error in request: " + response.statusText);
+      throw Error('Error in request: ' + response.statusText);
     }
   }
 
