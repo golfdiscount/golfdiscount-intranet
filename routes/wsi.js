ccf3d07e6171617a03478442f1d74255336867dc
Harmeet Singh <harmeet@golfdiscount.com>
2021-08-25 13:04:19 -0700

    Returns more than 1 item associated with an order if applicable

--- routes/wsi.js
+++ routes/wsi.js
@@ -12,7 +12,7 @@ router.get('/orders/:order_num', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
 
   try{
-    let qry = `SELECT wsi_order.order_num AS "Order Number",
+    let qry = `SELECT wsi_order.order_num,
       c.sold_to_name,
       c.sold_to_address,
       c.sold_to_city,
@@ -40,7 +40,39 @@ router.get('/orders/:order_num', (req, res) => {
     LEFT OUTER JOIN shipping_conf ON shipping_conf.pick_ticket_num = wsi_order.pick_ticket_num
     WHERE wsi_order.order_num = "${req.params.order_num}";`;
 
-    db.executeQuery(qry, (results, error) => {
+    db.executeQuery(qry, (qryResults, error) => {
+      let results = {
+        products: []
+      }
+      
+      for (let i = 0; i < qryResults.length; i++) {
+        let currentRecord = qryResults[i];
+        if (i === 0) {
+          results.order_num = currentRecord.order_num;
+          results.sold_to_name = currentRecord.sold_to_name;
+          results.sold_to_address = currentRecord.sold_to_address;
+          results.sold_to_city = currentRecord.sold_to_city;
+          results.sold_to_state = currentRecord.sold_to_state;
+          results.sold_to_country = currentRecord.sold_to_country;
+          results.sold_to_zip = currentRecord.sold_to_zip;
+          results.ship_to_name = currentRecord.ship_to_name;
+          results.ship_to_address = currentRecord.ship_to_address;
+          results.ship_to_city = currentRecord.ship_to_city;
+          results.ship_to_state = currentRecord.ship_to_state;
+          results.ship_to_country = currentRecord.ship_to_country;
+          results.ship_to_zip = currentRecord.ship_to_zip;
+          results.tracking_num = currentRecord.tracking_num;
+          results.sent_to_shipstation = currentRecord.sent_to_shipstation;
+        }
+
+        results.products.push({
+          sku: currentRecord.sku,
+          sku_name: currentRecord.sku_name,
+          quantity: currentRecord.quantity,
+          unit_price: currentRecord.unit_price
+        })
+      }
+
       res.status(200).json(results);
     });
   } catch (e) {
