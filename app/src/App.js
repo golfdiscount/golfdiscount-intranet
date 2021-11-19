9a9b09c906dfac1067e7e95cf6d3538f3787ba0d
Harmeet Singh <harmeet@golfdiscount.com>
2021-11-18 18:12:58 -0800

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Golf Discount Intranet</h1>
+        <nav>
+          <div>WSI</div>
+          <div>Titleist</div>
+        </nav>
+      </header>
+      <main>
+        <p>Main content goes here</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
