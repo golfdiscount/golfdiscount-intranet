d0ab1a869dc2f285f65364d415707c7bffffa808
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-11 14:50:43 -0800

    Added navigation bars

---
+++ app/src/components/navigation/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <img src='gd_logo.png' alt='Golf Discount logo'></img>
+      <div>
+        <h1>WSI</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ app/src/components/navigation/Side-Navbar.js
@@ -0,0 +1,13 @@
+import './Side-Navbar.css';
+
+function SideNavbar() {
+  return (
+    <div className='side-nav'>
+      <p>Order Viewer</p>
+      <p>Order Creator</p>
+      <p>Order Analytics</p>
+    </div>
+  )
+}
+
+export default SideNavbar;
