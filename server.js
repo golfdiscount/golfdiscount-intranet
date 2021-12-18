eca39d2c4f87da5fe7461d17ea99a5e4055e6eef
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 01:12:36 -0800

    Updated routes to be compliant with reverse proxy

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8000;
 let wsi = require('./routes/wsi');
 let analytics = require('./routes/analytics');
 
-app.use('/wsi', wsi);
-app.use('/analytics', analytics);
+app.use('/api/wsi', wsi);
+app.use('/api/analytics', analytics);
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.json());
