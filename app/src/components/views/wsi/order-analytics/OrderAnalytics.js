ef67ff72e7370a4ba6161832bf3a9f47cb732026
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 00:51:09 -0800

    Initial commit

---
+++ app/src/components/views/wsi/order-analytics/OrderAnalytics.js
@@ -0,0 +1,9 @@
+function OrderAnalytics() {
+  return (
+    <div className='tab-content'>
+      <p>Order analytics tab</p>
+    </div>
+  );
+}
+
+export default OrderAnalytics;
---
+++ app/src/components/views/wsi/order-creation/OrderCreator.js
@@ -0,0 +1,9 @@
+function OrderCreator() {
+  return (
+    <div className='tab-content'>
+      <p>Order creation tab</p>
+    </div>
+  );
+}
+
+export default OrderCreator;
