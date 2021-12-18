08260958ed52aba9021a185009fb2f6e5d0fb8cb
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 02:06:49 -0800

    Added search functionality

--- client/src/components/views/wsi/order-viewer/OrderViewer.js
+++ client/src/components/views/wsi/order-viewer/OrderViewer.js
@@ -3,14 +3,21 @@ import { useState } from 'react';
 
 function OrderViewer() {
   const [currentOrder, setCurrentOrder] = useState();
+  const [errors, setErrors] = useState([]);
   let order = <Order orderNum={currentOrder} />;
+  const errorMessages = errors.map(error => {
+    return (
+      <p style={{'color': 'red'}}>Error: {error}</p>
+    );
+  });
 
   return (
     <div className='tab-content'>
+      {errorMessages}
       <h1>Order Search</h1>
       <form onSubmit={(e) => {
           e.preventDefault();
-          getOrder(e.target.elements['orderNumber'].value, setCurrentOrder)
+          getOrder(e.target.elements['orderNumber'].value, setCurrentOrder, setErrors)
         }}>
         <label htmlFor='order-num'>Order Number:</label>
         <input required type='text' name='orderNumber'/>
@@ -34,8 +41,20 @@ function Order(props) {
  * @param {String} orderNumber
  * @param {Function} setState Function to update state of the current order in the OrderViewer component
  */
-function getOrder(orderNumber, setState) {
-  console.log(orderNumber);
+function getOrder(orderNumber, setState, setError) {
+  fetch(`/api/wsi/orders/${orderNumber}`)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+
+    return res.json();
+  })
+  .then(res => console.log(res))
+  .catch(err => {
+    setError([err.message]);
+  });
+  setError([]);
   setState(orderNumber);
 }
 
