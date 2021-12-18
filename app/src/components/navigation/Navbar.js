2df98a933cf18519ef223e002308982f4aa70ab6
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 00:51:28 -0800

    Navigation now has routing to WSI and landing page

--- app/src/components/navigation/Navbar.js
+++ app/src/components/navigation/Navbar.js
@@ -1,14 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
   return (
     <div className='navbar'>
-      <img src='gd_logo.png' alt='Golf Discount logo'></img>
-      <div>
-        <h1>WSI</h1>
-      </div>
+      <NavLink to=''>
+        <img src='gd_logo.png' alt='Golf Discount logo'></img>
+      </NavLink>
+      <NavLink className='tab navbar-tab' to='/wsi'>WSI</NavLink>
     </div>
-  )
+  );
 }
 
 export default Navbar;
