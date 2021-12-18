61c10cb04010d645aa2daf9fcbd5215ecca2b206
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 00:48:40 -0800

    Added routing to WSI view and a landing page

--- app/src/App.js
+++ app/src/App.js
@@ -3,27 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Components and style sheets
 import './App.css';
 import Navbar from './components/navigation/Navbar';
-import SideNavbar from './components/navigation/Side-Navbar';
 import Footer from './components/navigation/Footer';
-import OrderCreator from './components/order-creation/OrderCreator';
-import OrderViewer from './components/order-viewer/OrderViewer';
-import OrderAnalytics from './components/order-analytics/OrderAnalytics';
+import Landing from './components/views/landing';
+import Wsi from './components/views/wsi/wsi';
 
 function App() {
   return (
     <div className='app-container'>
-      <Navbar />
-      <div className='main-content'>
-        <BrowserRouter>
-          <SideNavbar />
-          <Routes>
-            <Route path='/' element={<OrderViewer />} />
-            <Route path='/order-creator' element={<OrderCreator />} />
-            <Route path='/order-analytics' element={<OrderAnalytics />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path ='' element={<Landing />} />
+          <Route path='/wsi/*' element={<Wsi />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
