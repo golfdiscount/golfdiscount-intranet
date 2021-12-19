fc27ae1a30310b21675ae2c55039465f6e0f1c5c
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-18 22:06:46 -0800

    Added product component

--- client/src/components/views/wsi/order-creation/OrderCreator.js
+++ client/src/components/views/wsi/order-creation/OrderCreator.js
@@ -1,7 +1,40 @@
+import './OrderCreator.css';
+import { useState } from 'react';
+
 function OrderCreator() {
+
+  function OrderInfo() {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+  function Product() {
+    const [products, addProduct] = useState([]);
+    return(
+      <div className='vertical-form'>
+        <label>
+          SKU:
+          <input required />
+        </label>
+        <label>
+          Quantity:
+          <input type='number' required />
+        </label>
+        <label>
+          Price:
+          <input type='number' required />
+        </label>
+      </div>
+    );
+  }
+
   return (
     <div className='tab-content'>
       <p>Order creation tab</p>
+      <Product />
+      <button type='button' onClick>Add another product</button>
     </div>
   );
 }
