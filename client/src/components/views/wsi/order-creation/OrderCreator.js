e5c2cfd14fc516bfccb3fe107d2afa5389296f85
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-19 00:38:18 -0800

    Added shipping method and product components

--- client/src/components/views/wsi/order-creation/OrderCreator.js
+++ client/src/components/views/wsi/order-creation/OrderCreator.js
@@ -2,27 +2,41 @@ import './OrderCreator.css';
 import { useState } from 'react';
 
 function OrderCreator() {
+  const [products, setProducts] = useState([<Product key={0}/>]);
+  console.log(products.length);
 
   function OrderInfo() {
     return (
-      <div>
+      <div className='vertical-form'>
+        <label className='text-input'>
+          Order Number:
+          <input required />
+        </label>
+        <label>
+          Shipping Method:
+          <select defaultValue={"FDXH"}>
+            <option value="FDXH">FedEx Home Delivery</option>
+            <option value="FXES">FedEx 3-Day</option>
+            <option value="FX2D">FedEx 2-Day</option>
+            <option value="FXSO">FedEx 1-Day</option>
+          </select>
+        </label>
       </div>
     );
   }
 
   function Product() {
-    const [products, addProduct] = useState([]);
     return(
       <div className='vertical-form'>
-        <label>
+        <label className='text-input'>
           SKU:
           <input required />
         </label>
-        <label>
+        <label className='text-input'>
           Quantity:
           <input type='number' required />
         </label>
-        <label>
+        <label className='text-input'>
           Price:
           <input type='number' required />
         </label>
@@ -32,9 +46,19 @@ function OrderCreator() {
 
   return (
     <div className='tab-content'>
-      <p>Order creation tab</p>
-      <Product />
-      <button type='button' onClick>Add another product</button>
+      <h1>Order Creation</h1>
+      <OrderInfo />
+      <h2>Customer Address</h2>
+      <h2>Recipient Address</h2>
+      <h2>Products</h2>
+      {products}
+      <button type='button' onClick={() => {
+        let productCopy = products.map(product => {
+          return product;
+        });
+        productCopy.push(<Product key={productCopy.length + 1}/>);
+        setProducts(productCopy);
+      }}>Add another product</button>
     </div>
   );
 }
