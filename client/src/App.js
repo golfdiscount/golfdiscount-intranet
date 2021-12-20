9e76f6c70a53dbc4eef02969ae1e6f477a7672f1
Harmeet Singh <harmeet@golfdiscount.com>
2021-12-20 15:52:15 -0800

    Fixed route for 404 redirects

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path ='' element={<Landing />} />
+          <Route path='*' element={<Landing />} />
           <Route path='/wsi/*' element={<Wsi />} />
         </Routes>
         <Footer />
