fa045461dd2fab487e5474bfe39b0de860544d1f
Harmeet Singh <harmeet@golfdiscount.com>
2022-01-12 17:05:08 -0800

    Added content to landing page

--- src/components/views/landing.js
+++ src/components/views/landing.js
@@ -1,10 +1,41 @@
 // Landing page of the application
+import SideNavbar from '../navigation/Side-Navbar';
 
 function Landing() {
+  const tabs = [
+    {text: 'WSI Order Viewer', route: '/wsi'},
+    {text: 'WSI Order Creator', route:'/wsi/order-creator'}
+  ]
   return (
-    <div className='main-content'>
-      Landing page
-    </div>
+    <main>
+      <SideNavbar tabs={tabs} header='Quick Links'/>
+      <div className='tab-content tab-inner-content'>
+        <h1>Golf Discount Intranet</h1>
+        <section>
+          <h2>Overview</h2>
+          <p>
+            This Intranet provides access to implementation of internal Golf Discount.
+            These resources include WSI order creation and other commonly used utilities.
+          </p>
+        </section>
+        <section>
+          <h2>Change Log</h2>
+          <ul>
+            <li>Added quick links to landing page</li>
+            <li>Added WSI order creation and order search</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Upcoming Changes</h2>
+          <ul>
+            <li>Authentication</li>
+            <li>Excel spreadsheet joiner</li>
+            <li>Magento integration</li>
+            <li>Business analytics platform</li>
+          </ul>
+        </section>
+      </div>
+    </main>
   );
 }
 
