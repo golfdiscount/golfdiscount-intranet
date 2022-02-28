fe3d62e2ebd6d73606dd0e1092b8df2431ee6acd
Harmeet Singh <harmeet@golfdiscount.com>
2022-02-27 16:56:03 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// API routers
+const api = require("./routes/index");
+
+app.use("/api", api);
+
+// Configuration for serving React files
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
