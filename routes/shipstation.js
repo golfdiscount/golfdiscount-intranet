56b41aecd221b230cf35a3ccf2a626accaec16ba
Harmeet Singh <harmeet@golfdiscount.com>
2022-02-28 21:47:14 -0800

    Changed response formatting to remove unnecessary keys

--- routes/shipstation.js
+++ routes/shipstation.js
@@ -18,7 +18,14 @@ router.get('/orders/:orderNum', (req, res) => {
     ssRes.on('data', data => { rawData += data });
 
     ssRes.on('end', () => {
-      res.status(200).json(JSON.parse(rawData));
+      let orders = JSON.parse(rawData);
+
+      if (orders.total == 0) {
+        res.status(404).send('The order could not be found, please check the order number and try again');
+      } else {
+        let order = formatOrder(orders.orders[0]);
+        res.status(200).json(order);
+      }
     });
   });
 
@@ -29,5 +36,40 @@ router.get('/orders/:orderNum', (req, res) => {
 
   ssReq.end();
 });
+
+/**
+ * Pulls important information like name, address, and products out
+ * of a ShipStation order and reformats it
+ * @param {Object} order Singular order object to extract information out of
+ * @returns Object containing basic order information
+ */
+function formatOrder(orderInfo) {
+  let order = {
+    orderNumber: orderInfo.orderNumber,
+    orderDate: orderInfo.orderDate,
+    email: orderInfo.customerEmail,
+    customerAddress: {
+      name: orderInfo.shipTo.name,
+      street: `${orderInfo.shipTo.street1} ${orderInfo.shipTo.street2}`,
+      city: orderInfo.shipTo.city,
+      state: orderInfo.shipTo.state,
+      zip: orderInfo.shipTo.postalCode,
+      country: orderInfo.shipTo.country,
+      phone: orderInfo.shipTo.phone
+    },
+    products: [],
+  }
+
+  orderInfo.items.forEach(product => {
+    order.products.push({
+      sku: product.sku,
+      productName: product.name,
+      imageUrl: product.imageUrl,
+      quantity: product.quantity
+    });
+  });
+
+  return order;
+}
 
 export default router;
