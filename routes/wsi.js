8ecd9db05bc4a82df79f427a4664cecf21a9b314
Harmeet Singh <harmeet@golfdiscount.com>
2022-03-21 15:45:16 -0700

    Added a route to GET a WSI order

--- routes/wsi.js
+++ routes/wsi.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import https from 'https';
 
 const router = express.Router();
 
 router.get('/orders/:orderNum', (req, res) => {
-    const orderNum = req.params.orderNum;
+const wsiUrl = new URL(`/api/orders/${req.params.orderNum}`, process.env.WSI);
 
-    res.send(orderNum);
+    https.get(wsiUrl, (serverRes) => {
+        let rawData = '';
+
+        serverRes.on('data', (d) => {
+            rawData += d;
+        });
+
+        serverRes.on('end', () => {
+            if (serverRes.statusCode == 404) {
+                res.status(404).send('Unable to find order');
+            } else {
+                res.status(200).json(JSON.parse(rawData));
+            }
+        });
+
+        serverRes.on('error', (err) => {
+            console.log(err);
+            res.status(500).send('Unable to get order from WSI database');
+        });
+    });
 });
 
 export default router;
