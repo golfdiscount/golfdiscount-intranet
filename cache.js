d92b123df374a1c5616200fefaea704e014826af
Harmeet Singh <harmeet@golfdiscount.com>
2022-03-28 18:33:35 -0700

    Updated credentials to use environment variables

--- cache.js
+++ cache.js
@@ -4,8 +4,8 @@ let cache;
 
 async function cacheConnect() {
     cache = createClient({
-        url: 'rediss://gdinterface.redis.cache.windows.net:6380',
-        password: 'W8WiPbJpnUjuqvsV92d2LbkxRzyPHeY2HAzCaBy8C0Y='
+        url: process.env.REDIS_URL,
+        password: process.env.REDIS_PASS
     });
     
     cache.on('error', (err) => console.log('Redis client error:', err));
