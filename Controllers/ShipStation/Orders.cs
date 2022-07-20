2152797e3a1a9473b7bf2835f6bd7af1c86782e2
Harmeet Singh <harmeet@golfdiscount.com>
2022-07-19 21:18:27 -0700

    Initial commit

---
+++ Controllers/ShipStation/Orders.cs
@@ -0,0 +1,98 @@
+﻿using intranet.Models;
+using intranet.Models.Magento;
+using intranet.Models.Shipstation;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace intranet.Controllers.ShipStation
+{
+    [ApiController]
+    [Route("api/shipstation/[controller]/{orderNumber?}")]
+    public class Orders : Controller
+    {
+        private readonly HttpClient ssClient;
+        private readonly HttpClient magentoClient;
+        private readonly JsonSerializerOptions jsonOptions;
+
+        public Orders(IHttpClientFactory clientFactory, JsonSerializerOptions jsonOptions)
+        {
+            ssClient = clientFactory.CreateClient("Shipstation");
+            magentoClient = clientFactory.CreateClient("Magento");
+            this.jsonOptions = jsonOptions;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string orderNumber)
+        {
+            HttpResponseMessage response = await ssClient.GetAsync($"/orders?orderNumber={orderNumber}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if ((int)response.StatusCode > 500)
+                {
+                    return new StatusCodeResult((int)response.StatusCode);
+                }
+
+                return new BadRequestObjectResult(await response.Content.ReadAsStringAsync());
+            }
+
+            HttpContent content = response.Content;
+            OrderList orders = JsonSerializer.Deserialize<OrderList>(await content.ReadAsStringAsync(), jsonOptions);
+
+            SsOrder ssOrder = null;
+
+            orders.Orders.ForEach(ssApiOrder =>
+            {
+                if (ssApiOrder.OrderNumber == orderNumber)
+                {
+                    ssOrder = ssApiOrder;   
+                }
+            });
+
+            if (ssOrder == null)
+            {
+                return new NotFoundObjectResult($"Could not find order {orderNumber}");
+            }
+
+            Order order = new()
+            {
+                OrderNumber = ssOrder.OrderNumber,
+                OrderDate = ssOrder.OrderDate,
+                OrderStatus = ssOrder.OrderStatus,
+                Email = ssOrder.CustomerEmail,
+                CustomerAddress = new()
+                {
+                    Name = ssOrder.ShipTo.Name,
+                    Street = $"{ssOrder.ShipTo.Street1} {ssOrder.ShipTo.Street2}",
+                    City = ssOrder.ShipTo.City,
+                    State = ssOrder.ShipTo.State,
+                    Zip = ssOrder.ShipTo.PostalCode,
+                    Country = ssOrder.ShipTo.Country,
+                    Phone = ssOrder.ShipTo.Phone
+                },
+                Products = new()
+            };
+
+            foreach(Item item in ssOrder.Items)
+            {
+                response = await magentoClient.GetAsync($"/api/products/{item.Sku}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MagentoProduct product = JsonSerializer.Deserialize<MagentoProduct>(await response.Content.ReadAsStringAsync(), jsonOptions);
+                    order.Products.Add(new()
+                    {
+                        Sku = product.Sku,
+                        ProductName = product.Name,
+                        ImageUrl = item.ImageUrl,
+                        Quantity = (int)item.Quantity,
+                        Upc = product.Upc,
+                        Verified = 0
+                    });
+                }
+            }
+
+            return new OkObjectResult(order);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System.Text;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles();
+builder.Services.AddSingleton(new JsonSerializerOptions()
+{
+    PropertyNameCaseInsensitive = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+});
+
+DefaultAzureCredential cred = new();
+Uri keyvaultUri = new(Environment.GetEnvironmentVariable("vault-uri"));
+SecretClient secretClient = new(keyvaultUri, cred);
+
+builder.Services.AddHttpClient("Magento", client =>
+{
+    KeyVaultSecret magentoUri = secretClient.GetSecret("magento-uri");
+    KeyVaultSecret magentoKey = secretClient.GetSecret("magento-key");
+
+    client.BaseAddress = new(magentoUri.Value);
+    client.DefaultRequestHeaders.Add("x-functions-key", magentoKey.Value);
+});
+
+builder.Services.AddHttpClient("Wsi", client =>
+{
+    KeyVaultSecret wsiUri = secretClient.GetSecret("wsi-uri");
+    client.BaseAddress = new(wsiUri.Value);
+});
+
+builder.Services.AddHttpClient("Shipstation", client =>
+{
+    KeyVaultSecret shipstationUri = secretClient.GetSecret("shipstation-uri");
+    KeyVaultSecret shipstationKey = secretClient.GetSecret("shipstation-key");
+    KeyVaultSecret shipstationSecret = secretClient.GetSecret("shipstation-secret");
+
+    client.BaseAddress = new(shipstationUri.Value);
+
+    string shipstationCreds = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{shipstationKey.Value}:{shipstationSecret.Value}"));
+    client.DefaultRequestHeaders.Authorization = new("Basic", shipstationCreds);
+});
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+} else
+{
+    app.UseHsts();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
