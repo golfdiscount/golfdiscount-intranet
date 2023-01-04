846a3e865830c71f64db7f5d6061e46fe958280d
Harmeet Singh <harmeet@golfdiscount.com>
2023-01-04 11:27:18 -0800

    Updated routes to match new file structure

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,7 +5,7 @@ import 'App.css';
 import Footer from 'components/Footer/Footer';
 import Navbar from 'components/Navbar/Navbar';
 import Landing from 'pages/Landing/Landing';
-import Wsi from 'pages/Wsi/Wsi';
+import Wsi from 'pages/Wsi';
 import Shipping from 'pages/Shipping/Shipping';
 
 function App() {
