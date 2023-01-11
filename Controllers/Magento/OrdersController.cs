a0621880c4c50f2bae6502968909e6df976c9044
Harmeet Singh <harmeet@golfdiscount.com>
2023-01-11 12:55:56 -0800

    Initial commit

---
+++ Controllers/Magento/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using intranet.Models.Magento;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace intranet.Controllers.Magento
+{
+    [Route("api/magento/[controller]/{orderNumber}")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly HttpClient _magentoClient;
+        private readonly JsonSerializerOptions _jsonOptions;
+
+        public OrdersController(IHttpClientFactory clientFactory, JsonSerializerOptions jsonOptions)
+        {
+            _magentoClient = clientFactory.CreateClient("Magento");
+            _jsonOptions = jsonOptions;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string orderNumber)
+        {
+            HttpResponseMessage response = await _magentoClient.GetAsync($"/api/orders/{orderNumber}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if ((int)response.StatusCode >= 500)
+                {
+                    return new StatusCodeResult(500);
+                }
+
+                return new NotFoundResult();
+            }
+
+            HttpContent content = response.Content;
+            MagentoOrderModel order = JsonSerializer.Deserialize<MagentoOrderModel>(await content.ReadAsStringAsync(), _jsonOptions);
+
+            return new OkObjectResult(order);
+        }
+    }
+}
---
+++ Models/Magento/MagentoOrderModel.cs
@@ -0,0 +1,31 @@
+﻿namespace intranet.Models.Magento
+{
+    public class MagentoOrderModel
+    {
+        public int Id { get; set; }
+
+        public string OrderNumber { get; set; }
+
+        public string State { get; set; }
+
+        public string Status { get; set; }
+
+        public string Shipping { get; set; }
+
+        public AddressModel Customer { get; set; }
+
+        public AddressModel Recipient { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public float OrderTotal { get; set; }
+
+        public float ShippingTotal { get; set; }
+
+        public List<MagentoProduct> Products { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
