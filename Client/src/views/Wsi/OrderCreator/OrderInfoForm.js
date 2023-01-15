4601a3a7ad6cbebf7212e6e3e8a06425d6b84d38
BuildTools <unconfigured@null.spigotmc.org>
2023-01-15 15:34:11 -0800

    State is managed using passed in props

--- Client/src/views/Wsi/OrderCreator/OrderInfoForm.js
+++ Client/src/views/Wsi/OrderCreator/OrderInfoForm.js
@@ -1,46 +1,58 @@
+import PropTypes from 'prop-types';
 import { React } from 'react';
 
-function OrderInfoForm() {
+function OrderInfoForm(props) {
+  const orderInfo = props.orderInfo;
+  const setOrderInfo = props.setOrderInfo;
+
+  function handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const propertyName = target.name;
+
+    setOrderInfo({...orderInfo, [propertyName]: value});
+  }
+
   return (
     <div className='vertical-form'>
       <label>
         Store Number:
         <label>
-          <input type='radio' name='store_num' value='1' required defaultChecked/>
+          <input type='radio' name='storeNumber' value='1' required defaultChecked onChange={handleInputChange} />
           1
         </label>
         <label>
-          <input type='radio' name='store_num' value='2' />
+          <input type='radio' name='storeNumber' value='2' onChange={handleInputChange} />
           2
         </label>
         <label>
-          <input type='radio' name='store_num' value='3' />
+          <input type='radio' name='storeNumber' value='3' onChange={handleInputChange} />
           3
         </label>
         <label>
-          <input type='radio' name='store_num' value='5' />
+          <input type='radio' name='storeNumber' value='5' onChange={handleInputChange} />
           5
         </label>
         <label>
-          <input type='radio' name='store_num' value='6' />
+          <input type='radio' name='storeNumber' value='6' onChange={handleInputChange} />
           6
         </label>
         <label>
-          <input type='radio' name='store_num' value='7' />
+          <input type='radio' name='storeNumber' value='7' onChange={handleInputChange} />
           7
         </label>
       </label>
       <label className='text-input'>
         Order Number:
-        <input name='order_num' required />
+        <input name='orderNumber' value={orderInfo.orderNumber} onChange={handleInputChange} required />
       </label>
       <label>
         Order Date:
-        <input type='date' name='order_date' required></input>
+        <input type='date' name='orderDate' value={orderInfo.orderDate} required onChange={handleInputChange}></input>
       </label>
       <label>
         Shipping Method:
-        <select defaultValue={'FDXH'} name='shipping_method'>
+        <select defaultValue={'FDXH'} name='shippingMethod' onChange={handleInputChange}>
           <option value='FDXH'>FedEx Home Delivery</option>
           <option value='FXES'>FedEx 3-Day</option>
           <option value='FX2D'>FedEx 2-Day</option>
@@ -50,5 +62,15 @@ function OrderInfoForm() {
     </div>
   );
 }
+
+OrderInfoForm.propTypes = {
+  orderInfo: PropTypes.shape({
+    storeNumber: PropTypes.string,
+    orderNumber: PropTypes.string,
+    orderDate: PropTypes.string,
+    shippingMethod: PropTypes.string
+  }),
+  setOrderInfo: PropTypes.func
+};
 
 export default OrderInfoForm;
