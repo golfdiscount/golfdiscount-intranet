1af11ce0445740e0877657ee8c03c9a1c2032cfb
BuildTools <unconfigured@null.spigotmc.org>
2023-01-14 18:50:08 -0800

    Removed OrderInfo

--- Client/src/views/Wsi/OrderCreator/OrderCreator.js
+++ Client/src/views/Wsi/OrderCreator/OrderCreator.js
@@ -3,6 +3,7 @@ import { React, useState } from 'react';
 
 import AddressForm from 'components/FormComponents/AddressForm';
 import AddressModel from 'models/AddressModel';
+import OrderInfoForm from './OrderInfoForm';
 
 import './OrderCreator.css';
 
@@ -126,7 +127,7 @@ function OrderCreator() {
       <div className='order-creator tab-inner-content'>
         <div>
           <h1>Order Creation</h1>
-          <OrderInfo />
+          <OrderInfoForm />
           <h2>Customer Address</h2>
           <AddressForm address={customerAddress} setAddress={setCustomerAddress} />
           <label>
@@ -144,57 +145,6 @@ function OrderCreator() {
         </div>
       </div>
     </form>
-  );
-}
-
-function OrderInfo() {
-  return (
-    <div className='vertical-form'>
-      <label>
-        Store Number:
-        <label>
-          <input type='radio' name='store_num' value='1' required defaultChecked/>
-          1
-        </label>
-        <label>
-          <input type='radio' name='store_num' value='2' />
-          2
-        </label>
-        <label>
-          <input type='radio' name='store_num' value='3' />
-          3
-        </label>
-        <label>
-          <input type='radio' name='store_num' value='5' />
-          5
-        </label>
-        <label>
-          <input type='radio' name='store_num' value='6' />
-          6
-        </label>
-        <label>
-          <input type='radio' name='store_num' value='7' />
-          7
-        </label>
-      </label>
-      <label className='text-input'>
-        Order Number:
-        <input name='order_num' required />
-      </label>
-      <label>
-        Order Date:
-        <input type='date' name='order_date' required></input>
-      </label>
-      <label>
-        Shipping Method:
-        <select defaultValue={'FDXH'} name='shipping_method'>
-          <option value='FDXH'>FedEx Home Delivery</option>
-          <option value='FXES'>FedEx 3-Day</option>
-          <option value='FX2D'>FedEx 2-Day</option>
-          <option value='FXSO'>FedEx 1-Day</option>
-        </select>
-      </label>
-    </div>
   );
 }
 
