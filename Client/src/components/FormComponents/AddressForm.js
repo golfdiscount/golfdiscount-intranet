0117a74848b5cbd80f6d1b112880ca5182a01cde
BuildTools <unconfigured@null.spigotmc.org>
2023-01-15 00:12:26 -0800

    Form keeps track of its own state instead of using props

--- Client/src/components/FormComponents/AddressForm.js
+++ Client/src/components/FormComponents/AddressForm.js
@@ -1,44 +1,48 @@
-import React from 'react'; 
+import { React, useState } from 'react'; 
 import PropTypes from 'prop-types';
 
 function AddressForm(props) {
-  const address = props.address;
-  const setAddress = props.setAddress;
+  const [ address, setAddress ] = useState({
+    name: '',
+    street: '',
+    city: '',
+    state: '',
+    country: 'US',
+    zip: ''
+  });
 
   function handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const propertyName = target.name;
 
-    const newAddress = address;
-    newAddress[propertyName] = value;
-    setAddress(newAddress);
+    setAddress({...address, [propertyName]: value});
   }
 
   return (
     <fieldset className='vertical-form' disabled={props.disabled}>
       <label>
-                Name:
+        Name:
         <input value={address.name} name='name' onChange={handleInputChange} required />
       </label>
       <label>
-                Address:
+        Street:
         <input value={address.street} name='street' onChange={handleInputChange} required/>
       </label>
       <label>
-                City:
+        City:
         <input value={address.city} name='city' onChange={handleInputChange} required/>
       </label>
       <label>
-                State:
+        State:
         <input value={address.state} name='state' onChange={handleInputChange} required/>
       </label>
       <label>
-                Country:
+        Country:
         <input value={address.country} name='country' onChange={handleInputChange} required/>
       </label>
       <label>
-                Zip Code:
+        Zip Code:
         <input value={address.zip} name='zip' onChange={handleInputChange} required/>
       </label>
     </fieldset>
@@ -46,15 +50,6 @@ function AddressForm(props) {
 }
 
 AddressForm.propTypes = {
-  address: PropTypes.shape({
-    name: PropTypes.string,
-    street: PropTypes.string,
-    city: PropTypes.string,
-    state: PropTypes.string,
-    country: PropTypes.string,
-    zip: PropTypes.string
-  }),
-  setAddress: PropTypes.func,
   disabled: PropTypes.bool
 };
 
