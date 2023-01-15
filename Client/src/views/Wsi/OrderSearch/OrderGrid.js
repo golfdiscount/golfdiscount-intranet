ea6e9f49590cdfbe62b5a956d44118e3a1b2d591
BuildTools <unconfigured@null.spigotmc.org>
2023-01-14 17:19:26 -0800

    Added additional data field

--- Client/src/views/Wsi/OrderSearch/OrderGrid.js
+++ Client/src/views/Wsi/OrderSearch/OrderGrid.js
@@ -4,18 +4,20 @@ import { React } from 'react';
 import OrderGridEntry from './OrderGridEntry';
 
 function OrderGrid(props) {
-  const orders = props.orders.sort((a, b) => a.orderDate > b.orderDate ? 0 : 1);
+  const orders = props.orders.sort((a, b) => a.createdAt > b.createdAt ? 0 : 1);
   const orderGridEntries = orders.map(pickTicket => {
     const pickTicketNumber = pickTicket.pickTicketNumber;
     const orderNumber = pickTicket.orderNumber;
     const orderDate = new Date(pickTicket.orderDate + 'Z');
+    const orderCreationDate = new Date(pickTicket.createdAt + 'Z');
     const lineItemCount = pickTicket.lineItems.length;
 
     return <OrderGridEntry key={pickTicketNumber} 
       pickTicketNumber={pickTicketNumber} 
       orderNumber={orderNumber} 
       orderDate={orderDate}
-      lineItemCount={lineItemCount}/>;
+      lineItemCount={lineItemCount}
+      orderCreationDate={orderCreationDate}/>;
   });
 
 
@@ -27,6 +29,7 @@ function OrderGrid(props) {
           <th>Order Number</th>
           <th>Line Item Count</th>
           <th>Order Date</th>
+          <th>WSI Order Creation Time</th>
         </tr>
       </thead>
       <tbody>
