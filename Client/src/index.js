e02f161f95a8ace8f5f01d678fee30860995af79
Harmeet Singh <harmeet@golfdiscount.com>
2023-01-24 08:13:17 -0800

    Removed Redux store

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,16 +1,12 @@
 import App from './App';
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './store';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
